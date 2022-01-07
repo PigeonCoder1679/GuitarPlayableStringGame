fc86946892577814576d54d7bee544afdc61d74b
PigeonCoder1679 <97274758+PigeonCoder1679@users.noreply.github.com>
2022-01-07 11:31:23 +0530

    Add files via upload

---
+++ guitar.js
@@ -0,0 +1,29 @@
+function playE(){
+    var strE = document.getElementById("strE");
+    strE.play();
+}
+
+function playA(){
+    var strA = document.getElementById("strA");
+    strA.play();
+}
+
+function playD(){
+    var strD = document.getElementById("strD");
+    strD.play();
+}
+
+function playG(){
+    var strG = document.getElementById("strG");
+    strG.play();
+}
+
+function playB(){
+    var strB = document.getElementById("strB");
+    strB.play();
+}
+
+function playe(){
+    var stre = document.getElementById("stre");
+    stre.play();
+}
